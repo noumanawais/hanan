290e4b52156ebbb9b6eec903bc05cf3f22a0b0cc
noumanawais <miannomi123@gmail.com>
2013-12-06 05:35:39 -0500

    migrations

--- db/migrate/20131205100523_remove_friend_from_friend.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFriendFromFriend < ActiveRecord::Migration
-  def up
-    remove_column :friends, :friend
-  end
-
-  def down
-    add_column :friends, :friend, :string
-  end
-end
--- db/migrate/20131205100658_add_friend_email_to_friend.rb
+++
@@ -1,5 +0,0 @@
-class AddFriendEmailToFriend < ActiveRecord::Migration
-  def change
-    add_column :friends, :friend_email, :string
-  end
-end
